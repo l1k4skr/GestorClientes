9742d0a4a9ab86ebfb2dd113b705d8f7b46feec2
Andres Perez <andres.antonio2000@hotmail.com>
2022-10-03 15:34:43 -0300

    alpha 0.0.1

---
+++ python/databaseconn.py
@@ -0,0 +1,30 @@
+import mysql.connector
+
+class DatabaseConnection:
+    def __init__(self, host, user, password, database):
+        self.host = host
+        self.user = user
+        self.password = password
+        self.database = database
+
+    def __enter__(self):
+        self.conn = mysql.connector.connect(
+            host=self.host,
+            user=self.user,
+            password=self.password,
+            database=self.database
+        )
+        return self.conn
+    def cursor(self):
+        return self.__enter__().cursor()
+
+def intro_database(conexion, cursor, name, rut, phone, email):
+    cursor.execute(f"INSERT INTO clientes (name, rut, phone, email) VALUES (%s, %s, %s, %s)", (name, rut, phone, email))
+    conexion.commit()
+def conect(conect, name, passw, database):
+    conexion = DatabaseConnection(conect, name, passw, database)
+    return conexion
+    
+# if __name__ == '__main__':
+#     conexion = conect('localhost', 'root', '', 'lawyerdb')
+#     cursor = conexion.cursor()
---
+++ python/display.py
@@ -0,0 +1,81 @@
+from tkinter import ttk
+from tkinter import *   # Import tkinter
+import databaseconn
+
+try: 
+    with open('creds.txt', 'r') as r:
+        creds = r.readlines()
+        username = creds[0].rstrip()
+        passw = creds[1].rstrip()
+except:
+    raise Exception('Please create a creds.txt file with your username on the first line and password on the second line')
+def main():
+    root = Tk()
+    application = Product(root)
+    root.mainloop()
+    
+
+
+class Product:
+    def __init__(self, window):
+        self.wind = window
+        self.wind.title("Client Saver")
+        self.wind.geometry("900x500")
+        self.db = databaseconn.DatabaseConnection('localhost', username, passw, 'lawyerdb')
+        self.db_ = self.db.__enter__()
+        self.db_cursor =self.db_.cursor()
+        
+        # Creating a Frame Container
+        frame = LabelFrame(self.wind, text = "Register a new client")
+        frame.grid(padx=20)
+
+        # Name Input
+        Label(frame, text = "Name: ").grid(row = 0, column = 0)
+        self.name = Entry(frame)
+        self.name.grid(row = 0, column = 1, padx = 10, pady = 5, sticky = W + E)
+
+        # Rut Input
+        Label(frame, text = "Rut: ").grid(row = 1, column = 0)
+        self.rut = Entry(frame)
+        self.rut .grid(row = 1, column = 1, padx = 10, pady = 5, sticky = W + E)
+        
+        # Phone Input
+        Label(frame, text = "Phone: ").grid(row = 2, column = 0)
+        self.phone = Entry(frame)
+        self.phone.grid(row = 2, column = 1, padx = 10, pady = 5, sticky = W + E)
+        
+        # Email Input
+        Label(frame, text = "Email: ").grid(row = 3, column = 0)
+        self.email = Entry(frame)
+        self.email.grid(row = 3, column = 1, padx = 10, pady = 5, sticky = W + E)
+        
+        # Button Add Client
+        ttk.Button(frame, text = "Save client", command=self.send).grid(row = 4, columnspan = 2, sticky = W + E)
+        
+        
+        # Table
+        self.tree = ttk.Treeview(window, columns = ("#0", "#1", "#2", "#3"), show = "headings", height = 10)
+        self.tree.grid(row = 5, column = 0, pady=5, padx=5, columnspan = 2)
+        self.tree.heading("#1", text = "Name", anchor = CENTER)
+        self.tree.heading("#2", text = "Rut", anchor = CENTER)
+        self.tree.heading("#3", text = "Phone", anchor = CENTER)
+        self.tree.heading("#4", text = "Email", anchor = CENTER)
+        
+        self.displayData()
+        
+    def send(self):
+        self.name1 = self.name.get()
+        self.rut2 = self.rut.get()
+        self.phone3 = self.phone.get()
+        self.email4 = self.email.get()
+        databaseconn.intro_database(self.db_,self.db_cursor,self.name1, self.rut2, self.phone3, self.email4)
+        self.tree.delete(*self.tree.get_children())
+        self.displayData()
+    def displayData(self):
+        self.db_cursor.execute("SELECT * FROM clientes")
+        rows = self.db_cursor.fetchall()
+        for row in rows:
+            row = row[1:]
+            self.tree.insert("", 0, values = row)
+# if '__main__' == __name__:
+main()
