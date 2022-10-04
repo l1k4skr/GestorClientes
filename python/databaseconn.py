2b0b08fee83f60558de29e3ed21b045b123498e2
Andres Perez <andres.antonio2000@hotmail.com>
2022-10-04 20:05:28 -0300

    Alpha 0.0.3

--- python/databaseconn.py
+++ python/databaseconn.py
@@ -1,5 +1,6 @@
 import mysql.connector
 
+
 class DatabaseConnection:
     def __init__(self, host, user, password, database):
         self.host = host
@@ -15,19 +16,26 @@ class DatabaseConnection:
             database=self.database
         )
         return self.conn
+
     def cursor(self):
         return self.__enter__().cursor()
+    
 
 def intro_database(conexion, cursor, name, rut, phone, email):
-    cursor.execute(f"INSERT INTO clientes (name, rut, phone, email) VALUES (%s, %s, %s, %s)", (name, rut, phone, email))
+    cursor.execute(
+        f"INSERT INTO clientes (name, rut, phone, email) VALUES (%s, %s, %s, %s)", (name, rut, phone, email))
     conexion.commit()
+
+
 def delet_client(conexion, cursor, id):
-    cursor.execute(f"TRUNCATE FROM clientes WHERE id = {id}")
+    cursor.execute(f"DELETE FROM clientes WHERE id = {id};")
     conexion.commit()
+
+
 def conect(conect, name, passw, database):
     conexion = DatabaseConnection(conect, name, passw, database)
     return conexion
-    
+
 # if __name__ == '__main__':
 #     conexion = conect('localhost', 'root', '', 'lawyerdb')
 #     cursor = conexion.cursor()
--- python/display.py
+++ python/display.py
@@ -2,90 +2,105 @@ from tkinter import ttk
 from tkinter import *   # Import tkinter
 import databaseconn
 
-try: 
-    with open('creds.txt', 'r') as r:
+
+try:
+    with open('../creds.txt', 'r') as r:  # !
         creds = r.readlines()
         username = creds[0].rstrip()
         passw = creds[1].rstrip()
 except:
-    raise Exception('Please create a creds.txt file with your username on the first line and password on the second line')
+    raise Exception(
+        'Please create a creds.txt file with your username on the first line and password on the second line')
+
+
 def main():
     root = Tk()
+
     application = Product(root)
     root.mainloop()
-    
 
 
 class Product:
     def __init__(self, window):
         self.wind = window
         self.wind.title("Client Saver")
-        self.wind.geometry("900x500")
-        self.db = databaseconn.DatabaseConnection('localhost', username, passw, 'lawyerdb')
+        self.wind.geometry("1012x425")
+        self.wind.minsize(1012, 425)
+
+        self.db = databaseconn.DatabaseConnection(
+            'localhost', username, passw, 'lawyerdb')
         self.db_ = self.db.__enter__()
-        self.db_cursor =self.db_.cursor()
-        
+        self.db_cursor = self.db_.cursor()
+
         # Creating a Frame Container
-        frame = LabelFrame(self.wind, text = "Register a new client")
-        frame.grid(padx=20, pady=20, row=0, column=0, sticky="nsew")
+        frame = LabelFrame(self.wind, text="Register a new client")
+        frame.grid(row=0, column=0, sticky=W,  padx=4, pady=4)
 
         # Name Input
-        Label(frame, text = "Name: ").grid(row = 0, column = 0)
+        Label(frame, text="Name: ").grid(row=0, column=0)
         self.name = Entry(frame)
-        self.name.grid(row = 0, column = 1, padx = 10, pady = 5, sticky = W + E)
+        self.name.grid(row=0, column=1)
 
         # Rut Input
-        Label(frame, text = "Rut: ").grid(row = 1, column = 0)
+        Label(frame, text="Rut: ").grid(row=1, column=0)
         self.rut = Entry(frame)
-        self.rut .grid(row = 1, column = 1, padx = 10, pady = 5, sticky = W + E)
-        
+        self.rut .grid(row=1, column=1, padx=10, pady=5, sticky=W + E)
+
         # Phone Input
-        Label(frame, text = "Phone: ").grid(row = 2, column = 0)
+        Label(frame, text="Phone: ").grid(row=2, column=0)
         self.phone = Entry(frame)
-        self.phone.grid(row = 2, column = 1, padx = 10, pady = 5, sticky = W + E)
-        
+        self.phone.grid(row=2, column=1, padx=10, pady=5, sticky=W + E)
+
         # Email Input
-        Label(frame, text = "Email: ").grid(row = 3, column = 0)
+        Label(frame, text="Email: ").grid(row=3, column=0)
         self.email = Entry(frame)
-        self.email.grid(row = 3, column = 1, padx = 10, pady = 5, sticky = W + E)
-        
+        self.email.grid(row=3, column=1, padx=10, pady=5, sticky=W + E)
+
         # Button Add Client
-        ttk.Button(frame, text = "Save client", command=self.send).grid(row = 4, columnspan = 2, sticky = W + E)
-        
-        
+        ttk.Button(frame, text="Save client", command=self.send).grid(
+            row=4, columnspan=5, sticky=W + E)
+
         # Table
-        self.tree = ttk.Treeview(window, columns = ("#0", "#1", "#2", "#3", "#4"), show = "headings", height = 10)
-        self.tree.grid(row = 5, column = 0, pady=5, padx=5, columnspan = 2)
-        self.tree.heading("#1", text = "id", anchor = CENTER)
-        self.tree.heading("#2", text = "Name", anchor = CENTER)
-        self.tree.heading("#3", text = "Rut", anchor = CENTER)
-        self.tree.heading("#4", text = "Phone", anchor = CENTER)
-        self.tree.heading("#5", text = "Email", anchor = CENTER)
-        
+        self.tree = ttk.Treeview(window, columns=(
+            "#0", "#1", "#2", "#3", "#4"), show="headings", height=10)
+        self.tree.grid(row=5, column=0, pady=5, padx=5, columnspan=1)
+        self.tree.heading("#1", text="id", anchor=CENTER)
+        self.tree.heading("#2", text="Name", anchor=CENTER)
+        self.tree.heading("#3", text="Rut", anchor=CENTER)
+        self.tree.heading("#4", text="Phone", anchor=CENTER)
+        self.tree.heading("#5", text="Email", anchor=CENTER)
+
         self.displayData()
-        
+
         # Button Delete Client
-        ttk.Button(self.wind , text = "Delete Client", command=self.delete_client).grid(row = 0, column= 1, sticky = W + E)
-        
-        
+        ttk.Button(self.wind, text="Delete Client", command=self.delete_client).grid(
+            row=6, column=0, columnspan=1, sticky=W + E, padx=5)
+
     def send(self):
         self.name1 = self.name.get()
         self.rut2 = self.rut.get()
         self.phone3 = self.phone.get()
         self.email4 = self.email.get()
-        databaseconn.intro_database(self.db_,self.db_cursor,self.name1, self.rut2, self.phone3, self.email4)
+        databaseconn.intro_database(
+            self.db_, self.db_cursor, self.name1, self.rut2, self.phone3, self.email4)
         self.tree.delete(*self.tree.get_children())
         self.displayData()
-    
+
     def delete_client(self):
-        databaseconn.delet_client(self.db_,self.db_cursor, self.tree.item(self.tree.selection())['values'][0])
+        databaseconn.delet_client(self.db_, self.db_cursor, self.tree.item(
+            self.tree.selection())['values'][5])
         self.tree.item(self.tree.focus())
         self.tree.delete(*self.tree.get_children())
         self.displayData()
+        # print(self.wind.winfo_reqwidth())
+        # print(self.wind.winfo_reqheight())
+
     def displayData(self):
         self.db_cursor.execute("SELECT * FROM clientes")
         rows = self.db_cursor.fetchall()
-        for n, row  in enumerate(rows):
-            self.tree.insert("", 0, values = [n] + list(row))
+        for n, row in enumerate(rows):
+            self.tree.insert("", 0, values=[n] + list(row[1:]) + [row[0]])
+
+
 # if '__main__' == __name__:
 main()
